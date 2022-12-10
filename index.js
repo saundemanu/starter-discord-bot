29a3f375707c085a8816870040956d93f0b83284
Emanuel Saunders <cs.emanuel42@gmail.com>
2022-12-09 21:59:43 -0500

    removed config.json requirement

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const fs = require('node:fs');
 const path = require('node:path');
 const { SlashCommandBuilder, Routes } = require('discord.js');
 const { REST } = require('@discordjs/rest');
-const { clientId, guildId, token } = require('./config.json');
 
 
 const axios = require('axios')
